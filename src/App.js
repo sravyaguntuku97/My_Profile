7cb4d065297f399f7784d896c0e806ad9642a828
sravyaguntuku97 <sravya97.developer@gmail.com>
2020-06-30 00:05:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Header from './Pages/Header/Header.js'
-import Footer from './Pages/Footer/Footer.js'
-import Form from "./Pages/Forms/form.js"
-import HomepageLayout from './Pages/props/Classprops.js'
+import HomepageLayout from './Pages/Main/MainProfile.js'
 
 function App() {
   return (
