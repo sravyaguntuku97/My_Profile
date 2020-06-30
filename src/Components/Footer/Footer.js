ca1ae694528b7e0e0467e8ffb89f8c90b993ecb2
sravyaguntuku97 <sravya97.developer@gmail.com>
2020-06-30 17:53:20 +0530

    changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,48 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-    Container,
-    Grid,
-    Header,
-    List,
-    Segment,
-  } from 'semantic-ui-react'
+  Container,
+  Grid,
+  Header,
+  List,
+  Segment,
+  Icon,
+} from "semantic-ui-react";
 export default class Footer extends Component {
-    render() {
-        return (
-            <Segment inverted vertical style={{ padding: '5em 0em' }}>
-            <Container>
-              <Grid divided inverted stackable>
-                <Grid.Row>
-                  <Grid.Column width={3}>
-                    <Header inverted as='h4' content='About' />
-                    <List link inverted>
-                      <List.Item as='a'>Sitemap</List.Item>
-                      <List.Item as='a'>Contact Us</List.Item>
-                      <List.Item as='a'>Religious Ceremonies</List.Item>
-                      <List.Item as='a'>Gazebo Plans</List.Item>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={3}>
-                    <Header inverted as='h4' content='Services' />
-                    <List link inverted>
-                      <List.Item as='a'>Banana Pre-Order</List.Item>
-                      <List.Item as='a'>DNA FAQ</List.Item>
-                      <List.Item as='a'>How To Access</List.Item>
-                      <List.Item as='a'>Favorite X-Men</List.Item>
-                    </List>
-                  </Grid.Column>
-                  <Grid.Column width={7}>
-                    <Header as='h4' inverted>
-                      Footer Header
-                    </Header>
-                    <p>
-                      Extra space for a call to action inside the footer that could help re-engage users.
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Container>
-          </Segment>
-        )
-    }
+  render() {
+    return (
+      <Segment inverted vertical style={{ padding: "5em 0em" }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={5}>
+                <Header inverted as="h4" content="About" />
+                <List link inverted>
+                  <List.Item as="a">FullName:Ratna Sravya Guntuku</List.Item>
+                  <List.Item as="a">D/O: G.Krishna Kumar</List.Item>
+                  <List.Item as="a">Date of Birth : 31st July 1997</List.Item>
+                  <List.Item as="a">Nationality : Indian</List.Item>
+                  <List.Item as="a">Marital Status : Unmarried</List.Item>
+                  <List.Item as="a">
+                    Known Languages : English, Telugu and Hind.
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <Header inverted as="h4" content="Contact" />
+                <List link inverted>
+                  <List.Item as="a">
+                    {" "}
+                    <Icon name="phone" size="large" /> &nbsp;&nbsp;91-9885112928
+                  </List.Item>
+                  <List.Item as="a">
+                    {" "}
+                    <Icon name="whatsapp" size="large" /> &nbsp;&nbsp;9885112928
+                  </List.Item>
+                  <List.Item href={"mailto:sravyaguntuku97@gmail.com"}>
+                    <Icon name="mail" size="large" />{" "}
+                    &nbsp;&nbsp;sravyaguntuku97@gmail.com
+                  </List.Item>
+                  
+                  <List.Item as="a">
+                    <Icon name="map marker alternate" size="large" />{" "}
+                    &nbsp;&nbsp;D/NO-72-2-13, Saidurganagar 5th street, opposite
+                    HYUNDAI showroom, Lalacheruvu, Rajahmundry.
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <Header as="h4" inverted>
+                  More Projects
+                </Header>
+                <p>
+                  NOTE : Some of the Sample projects are in the Github
+                  For More details about the Projects and my work
+                  Please visit <br/><br/>
+                  <a href="https://github.com/sravyaguntuku97" style={{color:"white"}}>
+                  <Icon name="github" size="large"/><b>https://github.com/sravyaguntuku97</b></a>
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+    );
+  }
 }
